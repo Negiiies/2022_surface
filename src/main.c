c326e7a4f3807132f6d4c012551a31dd79ceb787
LE Dylan <dylan.le@ecole-89.com>
2022-11-21 16:43:43 +0100

    update

--- src/main.c
+++ src/main.c
@@ -11,45 +11,42 @@ int main(void)
     t_bunny_position pos_b;
     int l;
     int h;
- 
-    l = 798;
-    h = 599;
+    int i;
+
+    l = 766;
+    h = 766;
     win = bunny_start(l, h, false, "fl: Open a window tutorial");
     px = bunny_new_pixelarray(l, h);
     origin.x = 0;
     origin.y = 0;
     pos_a.x = 0;
     pos_a.y = 0;
-    pos_b.x = l+1;
-    pos_b.y = h+1;
-    clear_pixelarray(px, GREEN);
-     int i;
-     int j;
-     i = 0;
-     j = 0;
-
-     while(pos_a.x <= l) {
-    stu_draw_line(px, &pos_a, &pos_b, BLACK);
-    bunny_blit(&win->buffer, &px->clipable, &origin);
-    bunny_display(win);
-    pos_a.x += 1;
-    pos_b.x -= 1;
-    i += 1;
-     }
-     /* pos_a.x = 0;
-        pos_a.y = 0;
-        pos_b.x = l+1;
-        pos_b.y = h+1;*/
-     while(pos_a.y <= h) {
-         stu_draw_line(px, &pos_b, &pos_a, BLACK);
-         bunny_blit(&win->buffer, &px->clipable, &origin);
-         bunny_display(win);
-         pos_a.y += 1;
-         pos_b.y -= 1;
-         j += 1;
-     }
-
-     usleep(1e6);
-     bunny_stop(win);
-     return (EXIT_SUCCESS);
+    pos_b.x = l + 1;
+    pos_b.y = h + 1;
+    clear_pixelarray(px, YELLOW);
+    i = 0;
+    while(pos_a.x <= l) {
+        stu_draw_line(px, &pos_a, &pos_b, i / 3);
+        bunny_blit(&win->buffer, &px->clipable, &origin);
+        bunny_display(win);
+        pos_a.x += 1;
+        pos_b.x -= 1;
+        i += 1;
+    }
+    i = l;
+    pos_a.x = l + 1;
+    pos_a.y = 0;
+    pos_b.x = 0;
+    pos_b.y = h + 1;
+    while(pos_a.y <= h) {
+        stu_draw_line(px, &pos_b, &pos_a, i / 3);
+        bunny_blit(&win->buffer, &px->clipable, &origin);
+        bunny_display(win);
+        pos_a.y += 1;
+        pos_b.y -= 1;
+        i -= 1;
+    }
+    usleep(1e6);
+    bunny_stop(win);
+    return (EXIT_SUCCESS);
 }
