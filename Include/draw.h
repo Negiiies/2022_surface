92a6ad1db272979e6ae06afbf38dca7b0f5974da
LE Dylan <dylan.le@ecole-89.com>
2022-11-21 16:10:57 +0100

    update

--- Include/draw.h
+++
@@ -1,9 +0,0 @@
-#idndef DRAW_H_
-#define DRAW_H_
-#include <lapin.h>
-
-t_bunny_position project_paralell(int x,
-                                  int y,
-                                  int z,
-                                  int coef);
-#endif /*DRAW_H_*/
